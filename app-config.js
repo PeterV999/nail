4183d055f3bae1eac5bbd480320f73c8b29399e2
PeterV999 <peter091021.v1@gmail.com>
2026-08-04 02:28:32 +0700

    Configure Supabase project

--- app-config.js
+++ app-config.js
@@ -1,6 +1,6 @@
 window.FAH_NAIL_CONFIG = {
   shopSlug: "fah-nail",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  ownerRedirectUrl: ""
+  supabaseUrl: "https://punzqhfrhdgimvmczspv.supabase.co",
+  supabaseAnonKey: "sb_publishable_fsJgeeaA6Pd304J0EQ4AGw_EYWp3qFh",
+  ownerRedirectUrl: "https://fah-nail-booking.pages.dev/owner.html"
 };
